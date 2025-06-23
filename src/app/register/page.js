05cd792b159a3ae3d865f6c340fd6f872ea1266b
rav39439 <rav39439@gmail.com>
2025-06-23 17:04:15 +0530

    Update page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,72 +1,3 @@
-// export default function Register() {
-//   return (
-//     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-//       <h1>New User Registration</h1>
-
-//       <form>
-//         <div class="grid gap-6 mt-6 md:grid-cols-2">
-//           <div>
-//             <label
-//               for="first_name"
-//               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-//             >
-//                 Name
-//             </label>
-//             <input
-//               type="text"
-//               id="first_name"
-//               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-//               placeholder="John"
-//               required
-//             />
-//           </div>
-          
-//         </div>
-//         <div class="mb-6">
-//           <label
-//             for="email"
-//             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-//           >
-//             Email address
-//           </label>
-//           <input
-//             type="email"
-//             id="email"
-//             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-//             placeholder="john.doe@company.com"
-//             required
-//           />
-//         </div>
-//         <div class="mb-6">
-//           <label
-//             for="password"
-//             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-//           >
-//             Password
-//           </label>
-//           <input
-//             type="password"
-//             id="password"
-//             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-//             placeholder="•••••••••"
-//             required
-//           />
-//         </div>
-
-        
-//         <button
-//           type="submit"
-//           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-//         >
-//           Submit
-//         </button>
-//       </form>
-
-//     <a className="mt-2" href="/login/" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Already have a account? Login</a>
-
-//     </main>
-//   );
-// }
 
 
 'use client'
@@ -88,7 +19,7 @@ export default function Register() {
     setLoading(true)
 
     try {
-      const res = await fetch('http://localhost:8000/register', {
+      const res = await fetch('https://python-image-credit.onrender.com/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password, username: name })
